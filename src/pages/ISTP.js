346b5666e6b7056fb9b51a287f43c159626856ed
Z0ro0 <chlrkdmf0805@gmail.com>
2023-06-03 14:10:30 +0900

    next test

---
+++ src/pages/ISTP.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import styles from '../styles/mbti.module.css';
+function ISTP(){
+    return(
+        <div>
+            <div>
+                <p className={styles.p}>안녕하세요</p>
+            </div>
+        </div>
+    );
+}
+export default ISTP;
