eb72e9f130136962d564d11a4aa6c1bff2d67028
chaemin <w2114@e-mirim.hs.kr>
2023-06-04 15:44:09 +0900

    버튼 클릭시.. 이벤트..

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,18 @@
-import ISTP from './ISTP'
-
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 export default function Home() {
+  function begin(){
+    alert('안녕');
+  }
   return (
-    <>
-      <ISTP />
-    </>
+    <div>
+      <Head>
+        <title>공주 테스트</title>
+      </Head>
+      <body>
+        <h1 className={styles.mainT}>나는 어떤 공주님일까?</h1>
+        <button type='button' className={styles.btn} onClick={begin}>시작하기</button>
+      </body>
+    </div>
   ) 
 }
