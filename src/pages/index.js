6aba44fde522819615dcd29789e16664b2bcdde7
Z0ro0 <chlrkdmf0805@gmail.com>
2023-06-15 11:59:55 +0900

    결과창 html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 // import MainPage from '../pages/MainPage';
-import Question from '../pages/Question';
+// import Question from '../pages/Question';
+import Mbti from '../pages/Mbti';
 
 export default function Home() {
   return (
     <>
       {/* <MainPage /> */}
-      <Question />
+      {/* <Question /> */}
+      <Mbti />
     </>
   )
 }
