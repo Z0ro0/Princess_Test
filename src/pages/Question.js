54a943a14508cee0966c9635f57209b7f0917efa
leehyeryeong <w2108@e-mirim.hs.kr>
2023-06-19 23:30:52 +0900

    마지막 질문까지 선택시 결과창으로

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,17 +1,22 @@
 import styles from '../styles/Question.module.css';
 import Head from 'next/head';
 import { useState } from 'react';
+import { useRouter } from 'next/router';
 import data from '../json/qnaList.json'
 
 export default function Question() {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [progress, setProgress] = useState(0);
+  const router = useRouter();
 
   const handleAnswerClick = (index) => {
     // 선택지를 클릭할 때마다 다음 문항으로 넘어감
     if (currentQuestion < data.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
       setProgress((prevProgress) => prevProgress + 1);
+    } else {
+      // 마지막 질문에서는 결과 창 페이지로 이동
+      router.push('/Mbti');
     }
   };
 
