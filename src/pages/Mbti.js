fad6a9f609a0b4c0b28903cf89aa15256cd3ef2e
leehyeryeong <w2108@e-mirim.hs.kr>
2023-06-20 14:10:35 +0900

    결과 화면 메인 이미지 띄우기

--- src/pages/Mbti.js
+++ src/pages/Mbti.js
@@ -6,8 +6,23 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import data from '../json/result.json';
 import twinkleheart from '../../public/img/twinkleheart.png';
-import princess from '../../public/img/princess.png';
-// import myFunction from './data/mbti';
+import ENFJ from '/public/img/Enfj.png';
+import ENFP from '/public/img/Enfp.png';
+import ENTJ from '/public/img/Entj.png';
+import ENTP from '/public/img/Entp.png';
+import ESFJ from '/public/img/Esfj.png';
+import ESFP from '/public/img/Esfp.png';
+import ESTJ from '/public/img/Estj.png';
+import ESTP from '/public/img/Estp.png';
+import INFJ from '/public/img/Infj.png';
+import INFP from '/public/img/Infp.png';
+import INTJ from '/public/img/Intj.png';
+import INTP from '/public/img/Intp.png';
+import ISFJ from '/public/img/Isfj.png';
+import ISFP from '/public/img/Isfp.png';
+import ISTJ from '/public/img/Istj.png';
+import ISTP from '/public/img/Istp.png';
+import princessImg from '/public/img/princess.png'
 
 export default function Question() {
     const router = useRouter();
@@ -25,13 +40,70 @@ export default function Question() {
     const description2_4 = mbtiInfo?.description2_4 || '';
     const department = mbtiInfo?.department || '';
     const good = mbtiInfo?.good || '';
-    const bad = mbtiInfo?.bad || '';    
+    const bad = mbtiInfo?.bad || ''; 
 
     useEffect(() => {
         if (mbtiResult) {
           console.log('MBTI 결과:', mbtiResult);
         }
-      }, [mbtiResult]);
+    }, [mbtiResult]);
+
+    const getImageByType = (mbtiResult) => {
+        switch (mbtiResult) {
+            case 'ENFJ':
+                return ENFJ;
+                break;
+            case 'ENFP':
+                return ENFP;
+                break;
+            case 'ENTJ':
+                return ENTJ;
+                break;
+            case 'ENTP':
+                return ENTP;
+                break;
+            case 'ESFJ':
+                return ESFJ;
+                break;
+            case 'ESFP':
+                return ESFP;
+                break;
+            case 'ESTJ':
+                return ESTJ;
+                break;
+            case 'ESTP':
+                return ESTP;
+                break;
+            case 'INFJ':
+                return INFJ;
+                break;
+            case 'INFP':
+                return INFP;
+                break;
+            case 'INTJ':
+                return INTJ;
+                break;
+            case 'INTP':
+                return INTP;
+                break;
+            case 'ISFJ':
+              return ISFJ;
+              break;
+            case 'ISFP':
+              return ISFP;
+              break;
+            case 'ISTJ':
+              return ISTJ;
+              break;
+            case 'ISTP':
+              return ISTP;
+              break;
+            default:
+              return null; // 기본 이미지 또는 오류 처리
+        }
+    };
+
+    const mbtiImage = getImageByType(mbtiResult);
 
     return (
         <div className={styles.box}>
@@ -55,7 +127,7 @@ export default function Question() {
 
                             <div className={styles.p_title}>{title}</div>
 
-                            <Image src={princess} className={styles.p_img}/>
+                            {mbtiImage && <Image src={mbtiImage} alt="Princess" className={styles.princessImg}/>}
 
                             <div className={styles.p_title2}>{title2}</div>
 
@@ -79,11 +151,11 @@ export default function Question() {
                             <div className={styles.match}>
                                 <div className={styles.good_match}>
                                     <div className={styles.p_good_title}>{good}</div>
-                                    <Image src={princess} className={styles.p_good_img}/>
+                                    <Image src={princessImg} alt="good_match_princess" className={styles.p_good_img} />
                                 </div>
                                 <div className={styles.bad_match}>
                                     <div className={styles.p_bad_title}>{bad}</div>
-                                    <Image src={princess} className={styles.p_bad_img}/>
+                                    <Image src={princessImg} alt="bad_match_princess" className={styles.p_bad_img} />
                                 </div>
                             </div>
                         </div>{/* 버튼 빼고 모든 요소 */}
